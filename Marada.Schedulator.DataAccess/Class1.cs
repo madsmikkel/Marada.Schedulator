f0af71081715ccf5ac0cdcc9f31dc11264d340c6
Mads Mikkel Rasmussen <mads.mikkel@outlook.com>
2017-02-04 18:00:35 +0100

    Added DataAccess class lib.

---
+++ Marada.Schedulator.DataAccess/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marada.Schedulator.DataAccess
+{
+    public class Class1
+    {
+    }
+}
